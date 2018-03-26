744bd2d410f57e9a9ca254f0051471d5a86bf193
queziaa <queziaa31@gmail.com>
2018-03-27 07:49:52 +0800

    ‘0.0002’

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
+from django.http import Http404
+
 
 # Create your views here.
 def home(request):
-	return render(request, 'home.html', {'post_list' : post_list})
+    post_list = Article.objects.all()  #获取全部的Article对象
+    return render(request, 'home.html', {'post_list' : post_list})
 
-
-def years(request,year):
-	post = Article.objects.all()[int(year)]
-	str = ("title = %s, category = %s, date_time = %s, content = %s"  
-		% (post.title, post.category, post.date_time, post.content))
-	return HttpResponse(str)
-#
-def test(request) :
-    return render(request, 'test.html', {'current_time': datetime.now()})
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    return render(request, 'post.html', {'post' : post})
--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -13,15 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+from django.contrib import admin 
 from django.urls import path
 from article import views
-
+from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home),
-    path('<int:year>/', views.years),  
-    path('test/',views.test)
+    path('admin/',admin.site.urls),
+    path('', views.home, name = 'home'),
+    path('<int:id>/',  views.detail, name='detail'),
 
 ]
