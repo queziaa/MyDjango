d8ca5973c900179a87d257d44381484c15cefe9b
root <queziaa31@gmail.com>
2018-09-24 12:27:19 +0800

    小修复

--- my_blog/static/js/uploadimg.js
+++ my_blog/static/js/uploadimg.js
@@ -1,23 +1,29 @@
-var page = 0;
-var $right=$("#right");
-
-$(document).ready(function(){
-	setInterval(load_img_funct,500);
+$(document).ready(function () {
+	window["page"] = 0;
+	window["imgPrompt"] = $("#imgPrompt");
+	imgPrompt.attr("onclick","load_img_funct()");
+	$(document).ready(function(){
+		window["imgs_interval"] = setInterval(load_img_funct,500);
+	}); 
 });
 function load_img_funct(){
 	if($(document).scrollTop()>$(document).height()-$(window).height()-20){
+		imgPrompt.html("正在加载中...");
 		$.ajax({
 			type:"GET",
 			url:"/get_img/"+page,
 			dataType:"json",
 			success:function(imgs_json){
-				loadState = true;
 				page+=1;
 				for(var i=0;i<imgs_json.len;i++)
-					$right.append($('<div class="imfMod"><img src='+imgs_json.data[i].url+' title='+imgs_json.data[i].id+' ></div>'));
-				if(imgs_json.len<5)
-					$(window).scroll=null;
-				loadState = false;
+					imgPrompt.before('<div class="imfMod"><img src='+imgs_json.data[i].url+' title='+imgs_json.data[i].id+' onclick="enlarge(this)"></div>');
+				if(imgs_json.len<5){
+					clearInterval(imgs_interval);
+					imgPrompt.html("已经没有更多图片了");
+				}else{
+					imgPrompt.html("下拉或点击加载更多");
+					imgPrompt.css("position","static");
+				}
 			}
 		});
 	}
